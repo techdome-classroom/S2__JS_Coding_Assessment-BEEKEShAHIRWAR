14fb8ff6749908178c342bebe4ec6e5362434ab1
Beekesh Ahirwar <148740444+BEEKEShAHIRWAR@users.noreply.github.com>
2024-09-25 14:44:30 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,18 +4,7 @@
  */
 var romanToInt = function(s) {
     const romanValues = {
-        function romanToInt(s) {
-            const romanDict = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
-            let total = 0;
-            for (let i = 0; i < s.length - 1; i++) {
-                if (romanDict[s[i]] < romanDict[s[i + 1]]) {
-                    total -= romanDict[s[i]];
-                } else {
-                    total += romanDict[s[i]];
-                }
-            }
-            return total + romanDict[s[s.length - 1]];
-        }
+        
     }
 };
 
