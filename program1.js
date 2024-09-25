26808de917047fe25d0eebbcde708e374ea25fa8
Beekesh Ahirwar <148740444+BEEKEShAHIRWAR@users.noreply.github.com>
2024-09-25 14:57:52 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -34,40 +34,5 @@ module.exports = { isValid };
 
 
 
-    it("test case 1", function () {
-        const result = isValid("()");
-        assert.equal(true, result);
-    });
-
-    it("test case 2", function () {
-        const result = isValid("()[]{}");
-        assert.equal(true, result);
-    });
-
-    it("test case 3", function () {
-        const result = isValid("{[()]}");
-        assert.equal(true, result);
-    });
-
-    it("test case 4", function () {
-        const result = isValid("(]");
-        assert.equal(false, result);
-    });
-
-    it("test case 5", function () {
-        const result = isValid("([)]");
-        assert.equal(false, result);
-    });
-
-    it("test case 6", function () {
-        const result = isValid("");
-        assert.equal(true, result);
-    });
-
-    it("test case 7", function () {
-        const result = isValid("(){");
-        assert.equal(false, result);
-    });
-
-
+   
 
