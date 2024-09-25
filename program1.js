f423793d62409549bb1a1e7e1a4bed1ce8b8d668
Beekesh Ahirwar <148740444+BEEKEShAHIRWAR@users.noreply.github.com>
2024-09-25 14:35:23 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -13,7 +13,9 @@ var isValid = function(s) {
     for(let i=0;i<s.length;i++){
         let char = s[i];
 
-        if(map[char])
+        if(map[char]){
+            let topElement = stack.length
+        }
     }
 };
 
