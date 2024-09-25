b55e0f8798cc95d8ab3f6d0379c69955c6a6c4b8
Beekesh Ahirwar <148740444+BEEKEShAHIRWAR@users.noreply.github.com>
2024-09-25 15:00:40 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,51 +1,7 @@
 /**
- * @param {string} 
+ * @param {string} s
  * @return {boolean}
  */
-const {isValid} = require("./program1");
-const assert = require("assert");
-
-
-
-describe("test cases for problem 1 ", function () {
-
-    it("test case 1", function () {
-        const result = isValid("()");
-        assert.equal(true, result);
-    });
-
-    it("test case 2", function () {
-        const result = isValid("()[]{}");
-        assert.equal(true, result);
-    });
-
-    it("test case 3", function () {
-        const result = isValid("{[()]}");
-        assert.equal(true, result);
-    });
-
-    it("test case 4", function () {
-        const result = isValid("(]");
-        assert.equal(false, result);
-    });
-
-    it("test case 5", function () {
-        const result = isValid("([)]");
-        assert.equal(false, result);
-    });
-
-    it("test case 6", function () {
-        const result = isValid("");
-        assert.equal(true, result);
-    });
-
-    it("test case 7", function () {
-        const result = isValid("(){");
-        assert.equal(false, result);
-    });
-
-})
-
 
 var isValid = function(s) {
     let stack = [];
